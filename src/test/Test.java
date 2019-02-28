0ad6d2fc2b9fbdb8d6b0c2c1e49832102c709bdd
Mohamed Serhir <32732921+mvmser@users.noreply.github.com>
2019-02-28 15:15:33 +0100

    add test

---
+++ src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.print("test");
+
+	}
+
+}
