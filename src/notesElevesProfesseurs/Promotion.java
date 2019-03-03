c03ab307828d5e1f90616edab6b04b816f869c13
InesZrg <ineszarga@yahoo.fr>
2019-03-03 15:42:32 +0100

    promotion

--- src/notesElevesProfesseurs/Promotion.java
+++ src/notesElevesProfesseurs/Promotion.java
@@ -7,7 +7,7 @@ public class Promotion extends Eleve {
 	private String nom;
 	private ArrayList<Eleve> eleves;
 	
-	/* constructeur à 1 parametre*/
+	/* constructeur ï¿½ 1 parametre*/
 	public Promotion(String nom) {
 		this.nom = nom;
 	}
@@ -26,7 +26,7 @@ public class Promotion extends Eleve {
 	
 	public Eleve rechercher(int numId) {
 		for(int i = 0; i < eleves.size(); i++) {
-			if(this.eleves.get(i).getNumIdentification() == numId) {
+			if(this.eleves.get(i).getNumIdentifiant() == numId) {
 				return this.eleves.get(i);
 			}
 		}	
