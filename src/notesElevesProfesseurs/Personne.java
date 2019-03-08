257ee24a3ebeac960ee56a7946e6a0a481bab218
Mohamed Serhir <32732921+mvmser@users.noreply.github.com>
2019-03-08 09:20:02 +0100

    up com

--- src/notesElevesProfesseurs/Personne.java
+++ src/notesElevesProfesseurs/Personne.java
@@ -1,6 +1,9 @@
 package notesElevesProfesseurs;
 
-//Eleve et Professeur hérite de Personne car ils ont des propriétés communes (prenom et nom)
+/*
+ * Eleve et Professeur herite de Personne car ils ont des proprietes communes (prenom et nom)
+ * On utilise alors la fonctionnalite d'heritage.
+ */
 public abstract class Personne {
 
 	//---ATTRIBUTS---
