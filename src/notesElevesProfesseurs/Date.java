9f0426c0f16eda97e6dcb69aeea567b8a3500b24
Mohamed Serhir <32732921+mvmser@users.noreply.github.com>
2019-03-31 19:05:03 +0200

    Create class Date

--- src/notesElevesProfesseurs/Date.java
+++ src/notesElevesProfesseurs/Date.java
@@ -1,5 +1,13 @@
 package notesElevesProfesseurs;
 
 public class Date {
-
+	private int annee;
+	private int mois;
+	private int jour;
+	
+	public Date(int jour, int mois, int annee) {
+		this.jour = jour;
+		this.mois = mois;
+		this.annee = annee;
+	}
 }
