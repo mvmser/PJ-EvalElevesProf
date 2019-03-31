1f2b4ebd7b0508d5f20e0a4af313ae97ca34227b
Mohamed Serhir <32732921+mvmser@users.noreply.github.com>
2019-03-31 23:35:21 +0200

    update prof

--- src/notesElevesProfesseurs/Professeur.java
+++ src/notesElevesProfesseurs/Professeur.java
@@ -1,68 +1,62 @@
 package notesElevesProfesseurs;
 
-import java.util.Collections;
-import java.util.Iterator;
-
+/**
+ * 
+ * @author SERHIR, ZARGA
+ * @version 1.1
+ *
+ */
 public class Professeur extends Personne {
 
-	//---CONSTRUCTEURS---
+	/**
+	 * Constructeur de professeur
+	 * @param nom
+	 * @param prenom
+	 * @since 1.0
+	 */
 	public Professeur(String nom, String prenom) {
 		super(nom, prenom);
-	}
-	public Professeur() {
-		super();
+	}	
+	
+	/**
+	 * NEED TO UP
+	 * Permet de retrouver un eleve s'il existe
+	 * avec l'identifiant on peut retrouver la promotion car chque leve connait sa promotion
+	 * la methode rechercher de promotion permet de savoir si un eleve est dans cette promo
+	 * @param numId
+	 * @return l'eleve recherché sinon null
+	 * @since 1.1
+	 */
+	public Eleve rechercheEleve(String promotion, int numId) {
+		return null;
 	}
 	
-	
-	public double setNote(Promotion promo, int numId, int note, int indice) {
-
+	/**
+	 * NEED TO UP
+	 * @param promo
+	 * @param numId
+	 * @param note
+	 * @param indice
+	 * @since 1.0
+	 */
+	public void setNote(Promotion promo, int numId, int note, int indice) {
 		try {
-			Eleve eleve = rechercheEleve(numId);
+			//Eleve eleve = rechercheEleve(numId);
 			//Si la note d'indice i existe alors elle est modifier
 			//eleve.getNotes().get(indice);
 			
-			
-			}catch(IllegalStateException e) {
-				System.out.println("L'eleve numero: " + numId + " n'existe pas");
-			}
-
-		return 1.0; 
+		}catch(IllegalStateException e) {
+			System.out.println("L'eleve numero: " + numId + " n'existe pas");
 		}
-	
-	
-	//---RECHERCHER ELEVE---\\
-	public Eleve rechercheEleve(int numId) {
-		Promotion promo = new Promotion(nom);
-		Eleve eleve = new Eleve();
-		try{
-			for(int i = 0; i < promo.getEleves().size(); i++) {
-//				if(this.eleve.get(i).getNumIdentifiant() == numId) {
-//					return this.eleve.get(i);
-//	}
-			}			
-			for(int i = 0; i < promo.getEleves().size(); i++) {
-				if(eleve.getNumIdentifiant() == numId) {
-					return eleve;
-				}
-			}	
-		}
-		catch(IllegalStateException e) {
-			System.out.println(promo + " n'existe pas");
-		}
-		return null;
-		
 	}
 
-	
-	
-
-	
-	
-	//---TOSTRING---
+	/**
+	 *  Methode toString
+	 *  @return Un string, le nom le prenom d'un prof
+	 *  @since 1.0
+	 */
 	@Override
 	public String toString() {
 		return "(" + this.prenom + ", " + this.nom + ") ";
 	}
-	
-	
 }
