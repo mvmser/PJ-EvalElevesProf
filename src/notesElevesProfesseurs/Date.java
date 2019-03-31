7dc8204aa425cd414db5ef8c2f7cba5d5073a732
Mohamed Serhir <32732921+mvmser@users.noreply.github.com>
2019-03-31 19:06:16 +0200

    add getter in date

--- src/notesElevesProfesseurs/Date.java
+++ src/notesElevesProfesseurs/Date.java
@@ -10,4 +10,29 @@ public class Date {
 		this.mois = mois;
 		this.annee = annee;
 	}
+
+	public int getAnnee() {
+		return annee;
+	}
+
+	public void setAnnee(int annee) {
+		this.annee = annee;
+	}
+
+	public int getMois() {
+		return mois;
+	}
+
+	public void setMois(int mois) {
+		this.mois = mois;
+	}
+
+	public int getJour() {
+		return jour;
+	}
+
+	public void setJour(int jour) {
+		this.jour = jour;
+	}
+
 }
