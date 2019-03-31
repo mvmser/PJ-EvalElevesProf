6cde352cf0e21a26e617d2d828601f9d7759791f
Mohamed Serhir <32732921+mvmser@users.noreply.github.com>
2019-03-31 18:04:07 +0200

    Add comments on personne.java v1

--- src/notesElevesProfesseurs/Personne.java
+++ src/notesElevesProfesseurs/Personne.java
@@ -1,17 +1,20 @@
 package notesElevesProfesseurs;
 
 /*
- * Eleve et Professeur herite de Personne car ils ont des proprietes communes (prenom et nom)
+ * Eleve et Professeur heritent de Personne car ils ont des proprietes communes (prenom et nom)
  * On utilise alors la fonctionnalite d'heritage.
+ * 
+ * @author SERHIR, ZARGA
+ * @version 1.0
  */
 public abstract class Personne {
 
-	//---ATTRIBUTS---
+	//---ATTRIBUTS---\\
 	protected  String nom;
 	protected String prenom;
 	
 	
-	//---CONSTRUCTEURS---
+	//---CONSTRUCTEURS---\\
 	public Personne(String nom, String prenom) {
 		this.nom = nom;
 		this.prenom = prenom;
@@ -20,11 +23,22 @@ public abstract class Personne {
 		
 	}
 	
-	
-	//---ACCESSEURS : Getteurs--- 
+	/**
+	* Accesseur permettant de recuperer
+	* le nom de la personne
+	* @return le nom
+	* @since 1.0
+	*/
 	public String getNom(){
 		return nom;
 	}
+	
+	/**
+	* Accesseur permettant de recuperer
+	* le prenom de la personne
+	* @return le prenom
+	* @since 1.0
+	*/
 	public String getPrenom(){
 		return prenom; 
 	}
