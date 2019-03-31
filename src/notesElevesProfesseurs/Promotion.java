e3e4851fda2a1c39fe115d72251230971381bfcb
Mohamed Serhir <32732921+mvmser@users.noreply.github.com>
2019-03-31 21:51:48 +0200

    Updtate promotion: correct question 5

--- src/notesElevesProfesseurs/Promotion.java
+++ src/notesElevesProfesseurs/Promotion.java
@@ -2,44 +2,69 @@ package notesElevesProfesseurs;
 
 import java.util.ArrayList;
 
-/*
- * @authors SERHIR & ZARGA
+/**
+ * @author SERHIR, ZARGA
  * @version 1.0
  * + histoire de mutateur pour get eleves 
  */
 public class Promotion {
-
+	
+	/**Attributs*/
 	private String nom;
 	private ArrayList<Eleve> eleves = new ArrayList<Eleve>();
 	
-	/* constructeur a 1 parametre*/
+	/**
+	 * Constructeur d'une promotion
+	 * @param nom
+	 * @since 1.0
+	 */
 	public Promotion(String nom) {
 		this.nom = nom;
 	}
 	
+	/**
+	 * Permet de connaitre le nom de la promotion
+	 * @return le nom de la promotion
+	 * @since 1.0
+	 */
 	public String getNom() {
 		return nom;
 	}
 
+	/**
+	 * Permet de modifier le nom de la promotion
+	 * @param nom
+	 * @since 1.0
+	 */
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 
-	/*
+	/**
 	 * un accesseur en lecture getEleves. Justifier sa signature en commentaires dans le
 	 * code. Attention à ne pas violer le principe d’encapsulation : à justifier
+	 * @since 1.0
 	 */
 	public ArrayList<Eleve> getEleves() {
-		
 		return eleves;
 	}
 	
-	
+	/**
+	 * Permet d'ajouter un eleve à la promo
+	 * Verifier son utilité ? Demandé ?
+	 * @param eleve
+	 * @since 1.0
+	 */
 	public void setEleves(Eleve eleve) {
 		eleves.add(eleve);
 	}
 
-	//rechercher
+	/**
+	 * Permet de savoir si un eleve fait parti de la promotion
+	 * @param numId
+	 * @return L'eleve recherché, si pas trouvé retourne null
+	 * @since 1.0
+	 */
 	public Eleve rechercher(int numId) {
 		for(Eleve eleve: this.eleves) {
 			if(eleve.getNumIdentifiant() == numId)
