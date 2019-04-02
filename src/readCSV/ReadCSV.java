7eb3c4024b4d85da1a126442a3b13bf4c0a78bb9
Mohamed Serhir <32732921+mvmser@users.noreply.github.com>
2019-04-03 00:34:08 +0200

    add comments and begin from readEval
    + correct resatEleves

--- src/readCSV/ReadCSV.java
+++ src/readCSV/ReadCSV.java
@@ -2,9 +2,7 @@ package readCSV;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -20,7 +18,16 @@ public class ReadCSV {
 	private final static String RESOURCES_PATH = "files/";
 	private final static String ELEVES_FILE_NAME = "eleves.csv";
 	
-	public static Eleve readElevesFromCSV(){
+	/**
+	 * Permet de lire toutes les lignes d'un fichier eleves
+	 * exclu les lignes commentées avec #
+	 * Parcours les lignes et separe chaque ligne en 5 dans un tableau
+	 * On retrouve alors le Nom, Prenom, jour, mois, et l'annee
+	 * On cree alors un eleve avec ces infos et on l'ajout a notre arraylist
+	 * @return Une list d'eleves
+	 * @since 1.0
+	 */
+	public static List<Eleve> readElevesFromCSV(){
 		try {
 			Scanner inFile = new Scanner(new File(RESOURCES_PATH + ELEVES_FILE_NAME), "utf8");
 			
@@ -30,17 +37,36 @@ public class ReadCSV {
 				
 				if(value.charAt(0) != '#') {
 					String[] array = value.split(",");
-					eleves.add(new Eleve(array[0], array[1], Integer.parseInt(array[2]), 
+					try {
+						eleves.add(new Eleve(array[0], array[1], Integer.parseInt(array[2]), 
 								Integer.parseInt(array[3]), Integer.parseInt(array[4]) ));
+					}catch (NumberFormatException nfe) {
+						System.out.println("NumberFormatException: " + nfe.getMessage());
+					}
+					
 				}
 			}
 			inFile.close();
-			return null;
+			return eleves;
 		} catch (FileNotFoundException e) {
 			System.out.println("ERROR: " + e.getMessage());
 		}
 		return null;
 	}
+	
+	/**
+	 * Permet de lire toutes les lignes d'un fichier notes
+	 * exclu les lignes commentés avec #
+	 * @return Une list d'evaluations 
+	 * @since 1.0
+	 */
+	public static List<Eleve> readEvaluationsFromCSV(){
+		return null;
+	}
+	
+	
+	
+	
 	
 	/**
 	 * BufferedReader br = new BufferedReader(new FileReader(file));
