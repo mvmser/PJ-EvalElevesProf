d5546933b312c3930432a0520f3dddc1b191aeae
Mohamed Serhir <32732921+mvmser@users.noreply.github.com>
2019-04-03 00:13:15 +0200

    Updtate read csv
    can read eleves and add attributs in arrayllist

--- src/readCSV/ReadCSV.java
+++ src/readCSV/ReadCSV.java
@@ -1,21 +1,55 @@
 package readCSV;
 
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
+import notesElevesProfesseurs.Eleve;
+
+/**
+ * @author SERHIR, ZARGA
+ * @version 1.0
+ */
 public class ReadCSV {
 
-	private final static String RESOURCES_PATH = "src/main/resources/";
+	/** Attributs */
+	private final static String RESOURCES_PATH = "files/";
 	private final static String ELEVES_FILE_NAME = "eleves.csv";
-	 
-	public ReadCSV() {
-
+	
+	public static Eleve readElevesFromCSV(){
+		try {
+			Scanner inFile = new Scanner(new File(RESOURCES_PATH + ELEVES_FILE_NAME), "utf8");
+			
+			List<Eleve> eleves = new ArrayList<Eleve>();
+			while (inFile.hasNext()) {
+				String value = inFile.nextLine();
+				
+				if(value.charAt(0) != '#') {
+					String[] array = value.split(",");
+					eleves.add(new Eleve(array[0], array[1], Integer.parseInt(array[2]), 
+								Integer.parseInt(array[3]), Integer.parseInt(array[4]) ));
+				}
+			}
+			inFile.close();
+			return null;
+		} catch (FileNotFoundException e) {
+			System.out.println("ERROR: " + e.getMessage());
+		}
+		return null;
 	}
+	
+	/**
+	 * BufferedReader br = new BufferedReader(new FileReader(file));
+		String line;
+		while ((line = br.readLine()) != null) {
+		   // process the line.
+		}
+		br.close();
+	 */
 
-	public static void main(String[] args) {
-		File file = new File(RESOURCES_PATH + ELEVES_FILE_NAME);
-        //FileReader fr = new FileReader(file);
-        
-	}
 
 }
