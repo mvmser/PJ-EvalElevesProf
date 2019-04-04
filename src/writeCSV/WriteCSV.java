70f73c45d1cdbde4cdc4ccaa053ef8285282a65e
Mohamed Serhir <32732921+mvmser@users.noreply.github.com>
2019-04-04 09:19:33 +0200

    Create WriteCSV package

---
+++ src/writeCSV/WriteCSV.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package writeCSV;
+
+/**
+ * @author SERHIR, ZARGA
+ * @version 1.0
+ */
+public class WriteCSV {
+
+	/**
+	 * 
+	 */
+	public WriteCSV() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
