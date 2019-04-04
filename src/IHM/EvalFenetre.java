faa2eba918d3b51822f275126b2d36db678b7d50
ineszrg <44270892+ineszrg@users.noreply.github.com>
2019-04-04 11:30:55 +0200

    up

--- src/IHM/EvalFenetre.java
+++ src/IHM/EvalFenetre.java
@@ -1,17 +1,21 @@
 package IHM;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-public class EvalFenetre extends JFrame{
+
+
+
+public class EvalFenetre extends JFrame implements ActionListener{
 	
 	private static final long serialVersionUID = 1L;
 	
-	private JLabel labelNom, labelPrenom, labelAnnee;
-	private JTextField textFieldNom, textFieldPrenom, textField, textFieldAnnee;
-	private JPanel panel, panelSaisie, panelResult, panelButtons, panelNom, panelPrenom, panelNaissance;
-	private JButton buttonAgeCalcul, buttonReverse;
-	private int anneeCours;
+	private JLabel labelTitre;
+	private JPanel panel, panelMenu, panelBody;
+	private JButton buttonProfesseur, buttonEleve, buttonClassement;
 	
 	
 	public EvalFenetre() {
@@ -21,7 +25,7 @@ public class EvalFenetre extends JFrame{
 
 	public void build() {
 		this.setTitle("Evaluation Eleve / Professeur"); 
-		this.setSize(1200,800);
+		this.setSize(1500,800);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null); //regarder c quoi
 		this.setResizable(true); 
@@ -31,12 +35,67 @@ public class EvalFenetre extends JFrame{
 
 
 	private JPanel buildContentPane(){
-		panelSaisie = new JPanel();
-		panelSaisie.setLayout(new BorderLayout());
+		/** 
+		 * CREATION DU PANEL MENU EN WEST
+		 */
+		panelMenu = new JPanel();
 		
-		panelNom = new JPanel();
-		panelNom.setLayout(new BorderLayout());
+		GridLayout gl = new GridLayout(3,1);
+		gl.setVgap(150);
+		panelMenu.setLayout(gl);
 		
-		return panelSaisie;
+		/** Creation des 3 boutons du menu */
+		buttonProfesseur = new JButton("Professeur");
+		buttonProfesseur.addActionListener(this);
+		buttonProfesseur.setPreferredSize(new Dimension(200, 150));
+		panelMenu.add(buttonProfesseur);
+		
+		buttonEleve = new JButton("Eleve");
+		buttonEleve.addActionListener(this);
+		buttonEleve.setPreferredSize(new Dimension(300, 150));
+		panelMenu.add(buttonEleve);
+		
+		buttonClassement = new JButton("Classement des eleves");
+		buttonClassement.addActionListener(this);
+		buttonClassement.setPreferredSize(new Dimension(300, 150));
+		panelMenu.add(buttonClassement);
+		
+		
+		
+		
+		/** 
+		 * CREATION DU PANEL BODY EN EAST
+		 */
+		panelBody = new JPanel();
+		panelBody.setLayout(new BorderLayout());
+		
+		/** Creation du JLabel pour le titre */
+		labelTitre = new JLabel("Accueil");
+		Font f = new Font("Lato", Font.PLAIN, 45);
+		labelTitre.setFont(f);
+		panelBody.add(labelTitre, BorderLayout.CENTER);
+		
+		
+		
+		/** set colors*/
+		panelMenu.setBackground(Color.red);
+		panelBody.setBackground(Color.green);
+
+		
+		/**
+		 * CREATION DU PANEL TOTAL
+		 */
+		panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.add(panelMenu, BorderLayout.WEST); /** a gauche */
+		panel.add(panelBody, BorderLayout.CENTER); /** au centre */
+		
+		return panel;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
