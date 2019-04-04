f84cbddd6b49d491a275c085cd18169a4deb5c84
Mohamed Serhir <32732921+mvmser@users.noreply.github.com>
2019-04-04 11:45:27 +0200

    Update writeCSV

--- src/writeCSV/WriteCSV.java
+++ src/writeCSV/WriteCSV.java
@@ -3,25 +3,57 @@
  */
 package writeCSV;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import notesElevesProfesseurs.Professeur;
+
 /**
  * @author SERHIR, ZARGA
  * @version 1.0
  */
 public class WriteCSV {
 
-	/**
-	 * 
-	 */
-	public WriteCSV() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param args
-	 */
+	/** Attributs */
+	private final static String RESOURCES_PATH = "files/";
+	private final static String ELEVES_FILE_NAME = "eleves.csv";
+	private final static String PROFESSEURS_FILE_NAME = "professeurs.csv";
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	
+	/**
+	 * @since 1.0
+	 */
+	public static boolean writeEleveToCSV(){
+		try {
+			Scanner inFile = new Scanner(new File(RESOURCES_PATH + PROFESSEURS_FILE_NAME), "utf8");
+			
+			List<Professeur> professeurs = new ArrayList<Professeur>();
+			while (inFile.hasNext()) {
+				String value = inFile.nextLine();
+				
+				if(value.charAt(0) != '#') {
+					String[] array = value.split(",");
+					try {
+						professeurs.add(new Professeur(array[0], array[1]));
+					}catch (NumberFormatException nfe) {
+						System.out.println("NumberFormatException: " + nfe.getMessage());
+					}
+				}
+			}
+			inFile.close();
+			return true;
+		} catch (FileNotFoundException e) {
+			System.out.println("ERROR: " + e.getMessage());
+		}
+		return false;
+	}
+	
 
 }
