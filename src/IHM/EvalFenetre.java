fabacc9d5ec0846552a0db8719aee1f7b4e75ce0
InesZrg <ineszarga@yahoo.fr>
2019-04-07 16:24:52 +0200

    ajout des diffentes pages des button

--- src/IHM/EvalFenetre.java
+++ src/IHM/EvalFenetre.java
@@ -17,8 +17,8 @@ public class EvalFenetre extends JFrame implements ActionListener{
 	
 	private JLabel labelTitre, labelTitreSec, labelFooter;
 	private JPanel panel, panelMenu, panelBody, panelTitre, panelImage, panelFooter;
-	private JPanel panelBodyCard, panelProfesseur;
-	private JButton buttonProfesseur, buttonEleve, buttonClassement;
+	private JPanel panelBodyCard, panelProfesseur, panelEleve, panelClassement;
+	private JButton buttonProfesseur, buttonEleve, buttonClassement, buttonRetour;
 	
 	
 	public EvalFenetre() {
@@ -64,27 +64,27 @@ public class EvalFenetre extends JFrame implements ActionListener{
 		
 		/** Creation des 3 boutons du menu */
 		buttonProfesseur = new JButton("Professeur");
-		buttonProfesseur.setForeground(Color.BLACK);
-		buttonProfesseur.setBackground(new Color(219,164,164)); 
-		buttonProfesseur.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		buttonProfesseur.addActionListener(this);
-		buttonProfesseur.setFocusPainted(false); 
+			buttonProfesseur.setForeground(Color.BLACK);
+			buttonProfesseur.setBackground(new Color(219,164,164)); 
+			buttonProfesseur.setFont(new Font("Tahoma", Font.PLAIN, 15));
+			buttonProfesseur.addActionListener(this);
+			buttonProfesseur.setFocusPainted(false); 
 		panelMenu.add(buttonProfesseur);
 		
 		buttonEleve = new JButton("Eleve");
-		buttonEleve.setForeground(Color.BLACK); 
-		buttonEleve.setBackground(new Color(219,164,164));
-		buttonEleve.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		buttonEleve.addActionListener(this);
-		buttonEleve.setFocusPainted(false);
+			buttonEleve.setForeground(Color.BLACK); 
+			buttonEleve.setBackground(new Color(219,164,164));
+			buttonEleve.setFont(new Font("Tahoma", Font.PLAIN, 15));
+			buttonEleve.addActionListener(this);
+			buttonEleve.setFocusPainted(false);
 		panelMenu.add(buttonEleve);
 		
 		buttonClassement = new JButton("Classements des eleves");
-		buttonClassement.setForeground(Color.BLACK); 
-		buttonClassement.setBackground(new Color(219,164,164));
-		buttonClassement.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		buttonClassement.addActionListener(this);
-		buttonClassement.setFocusPainted(false);
+			buttonClassement.setForeground(Color.BLACK); 
+			buttonClassement.setBackground(new Color(219,164,164));
+			buttonClassement.setFont(new Font("Tahoma", Font.PLAIN, 15));
+			buttonClassement.addActionListener(this);
+			buttonClassement.setFocusPainted(false);
 		panelMenu.add(buttonClassement);
 		
 		/** Creation d'un label blanc pr faire une bordure en bas */
@@ -105,42 +105,42 @@ public class EvalFenetre extends JFrame implements ActionListener{
 		
 		/** Panel qui va contenir la 1er page */
 		panelBody = new JPanel();
-		panelBody.setLayout(new BorderLayout());
-		panelBody.setBackground(Color.WHITE);
-		
+			panelBody.setLayout(new BorderLayout());
+			panelBody.setBackground(Color.WHITE);
+			
 		/** Creation du JPanel qui va contenir les titres du projet */
 		panelTitre = new JPanel();
-		panelTitre.setLayout(new GridBagLayout());
-		panelTitre.setBackground(Color.WHITE);
+			panelTitre.setLayout(new GridBagLayout());
+			panelTitre.setBackground(Color.WHITE);
 		
 		/** Creation du JLabel pour le titre */
 		labelTitre = new JLabel();
-		labelTitre.setText("Accueil");
-		labelTitre.setFont(new Font("Tahoma", Font.BOLD, 35));
+			labelTitre.setText("Accueil");
+			labelTitre.setFont(new Font("Tahoma", Font.BOLD, 35));
 
 
 		/** Placement du labelTitre */
 		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.gridx = gbc.gridy = 0; 							/**  la grille commence en (0, 0)*/
-		gbc.gridwidth = GridBagConstraints.REMAINDER; 		/**  seul composant de sa colonne, il est donc le dernier. */
-		gbc.gridheight = 2; 								/**  valeur par défaut - peut s'étendre sur une seule ligne */
-		//gbc.anchor = GridBagConstraints.LINE_START; 		/**  ou BASELINE_LEADING mais pas WEST. */
-		gbc.insets = new Insets(70, 0, 20, 0); 				/**  Marge à gauche de 15 et marge au dessus de 70. */
+			gbc.gridx = gbc.gridy = 0; 							/**  la grille commence en (0, 0)*/
+			gbc.gridwidth = GridBagConstraints.REMAINDER; 		/**  seul composant de sa colonne, il est donc le dernier. */
+			gbc.gridheight = 2; 								/**  valeur par défaut - peut s'étendre sur une seule ligne */
+			//gbc.anchor = GridBagConstraints.LINE_START; 		/**  ou BASELINE_LEADING mais pas WEST. */
+			gbc.insets = new Insets(70, 0, 20, 0); 				/**  Marge à gauche de 15 et marge au dessus de 70. */
 		
 	
 		/** Creation d'un JLabel pour le second titre*/
 		labelTitreSec = new JLabel();
-		labelTitreSec.setText("Bienvenue dans le gestionnaire de note");
-		labelTitreSec.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		labelTitreSec.setForeground(new Color(152,152,152));
+			labelTitreSec.setText("Bienvenue dans le gestionnaire de note");
+			labelTitreSec.setFont(new Font("Tahoma", Font.PLAIN, 17));
+			labelTitreSec.setForeground(new Color(152,152,152));
 
 		panelTitre.add(labelTitre, gbc);
 		panelTitre.add(labelTitreSec);
 		
 		/** Creation du JPanel pour l'image */
 		panelImage = new JPanel();
-		panelImage.setLayout(new GridBagLayout());
-		panelImage.setBackground(Color.WHITE);
+			panelImage.setLayout(new GridBagLayout());
+			panelImage.setBackground(Color.WHITE);
 		
 		/** Creation de l'image */
 		ImageIcon icon = new ImageIcon(new ImageIcon("img.png").getImage().getScaledInstance(300, 150, Image.SCALE_DEFAULT));		
@@ -149,14 +149,14 @@ public class EvalFenetre extends JFrame implements ActionListener{
 		
 		/** Creation du JPanel pour le footer */
 		panelFooter = new JPanel();
-		panelFooter.setLayout(new GridBagLayout());
-		panelFooter.setBackground(Color.WHITE);
+			panelFooter.setLayout(new GridBagLayout());
+			panelFooter.setBackground(Color.WHITE);
 		
 		/** Creation d'un JLabel pour le footer*/
 		labelFooter = new JLabel();
-		labelFooter.setText("©  - ZARGA Inès");
-		labelFooter.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		labelFooter.setForeground(Color.BLACK);
+			labelFooter.setText("©  - ZARGA Inès");
+			labelFooter.setFont(new Font("Tahoma", Font.PLAIN, 15));
+			labelFooter.setForeground(Color.BLACK);
 		
 		panelFooter.add(labelFooter);
 		
@@ -182,31 +182,29 @@ public class EvalFenetre extends JFrame implements ActionListener{
 	 * @return panel
 	 */
 	public JPanel professeur(JPanel panel) {
-		
 		/** Panel qui va contenir la page professeur */
 		panelProfesseur = new JPanel();
-		panelProfesseur.setLayout(new BorderLayout());
-		panelProfesseur.setBackground(Color.WHITE);
+			panelProfesseur.setLayout(new BorderLayout());
+			panelProfesseur.setBackground(Color.WHITE);
 		
 		/** Panel du haut */
 		JPanel panel1P = new JPanel();
-		panel1P.setLayout(new GridBagLayout());
-		panel1P.setBackground(Color.WHITE);
+			panel1P.setLayout(new GridBagLayout());
+			panel1P.setBackground(Color.WHITE);
 		
-		JButton buttonRetour = new JButton("Retour");
+		buttonRetour = new JButton("Retour");
+			buttonRetour.setFocusPainted(false);
+
 	
 		JLabel labelTitreProf = new JLabel();
-		labelTitreProf.setText("Professeur");
-		labelTitreProf.setFont(new Font("Tahoma", Font.BOLD, 20));
+			labelTitreProf.setText("Professeur");
+			labelTitreProf.setFont(new Font("Tahoma", Font.BOLD, 20));
 		
 		/** Placement du labelTitreProf */
 		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.gridx = gbc.gridy = 0; 							
-		gbc.gridwidth = GridBagConstraints.REMAINDER; 		
-		gbc.gridheight = 2; 								
-		//gbc.anchor = GridBagConstraints.LINE_START; 		
-		gbc.insets = new Insets(20, 0, 20, 0); 				
-		
+			gbc.gridx = gbc.gridy = 0; 							
+			gbc.gridheight = 2; 								
+			gbc.insets = new Insets(60, 0, 20, 0); 				
 		
 		panel1P.add(buttonRetour);
 		panel1P.add(labelTitreProf, gbc);
@@ -226,8 +224,37 @@ public class EvalFenetre extends JFrame implements ActionListener{
 	 * @return
 	 */
 	public JPanel eleve(JPanel panel) {
+		/** Panel qui va contenir la page Eleve */
+		panelEleve = new JPanel();
+			panelEleve.setLayout(new BorderLayout());
+			panelEleve.setBackground(Color.WHITE);
 		
+		/** Panel du haut */
+		JPanel panel1P = new JPanel();
+			panel1P.setLayout(new GridBagLayout());
+			panel1P.setBackground(Color.WHITE);
 		
+		buttonRetour = new JButton("Retour");
+			buttonRetour.setFocusPainted(false);
+
+	
+		JLabel labelTitreEleve = new JLabel();
+			labelTitreEleve.setText("Eleve");
+			labelTitreEleve.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		/** Placement du labelTitreProf */
+		GridBagConstraints gbc = new GridBagConstraints();
+			gbc.gridx = gbc.gridy = 0; 							
+			gbc.gridheight = 2; 								
+			gbc.insets = new Insets(60, 0, 20, 0); 				
+		
+		panel1P.add(buttonRetour);
+		panel1P.add(labelTitreEleve, gbc);
+		
+		panelEleve.add(panel1P, BorderLayout.NORTH);
+		panelBodyCard.add(panelEleve, "ElevePage");
+		
+		panel.add(panelBodyCard, BorderLayout.CENTER); /** au centre */
 		setContentPane(panel);
 		return panel;
 	}
@@ -241,8 +268,37 @@ public class EvalFenetre extends JFrame implements ActionListener{
 	 * @return
 	 */
 	public JPanel classement(JPanel panel) {
+		/** Panel qui va contenir la page Eleve */
+		panelClassement = new JPanel();
+			panelClassement.setLayout(new BorderLayout());
+			panelClassement.setBackground(Color.WHITE);
+		
+		/** Panel du haut */
+		JPanel panel1P = new JPanel();
+			panel1P.setLayout(new GridBagLayout());
+			panel1P.setBackground(Color.WHITE);
+		
+		buttonRetour = new JButton("Retour");
+			buttonRetour.setFocusPainted(false);
+
 	
-	
+		JLabel labelTitreClassement = new JLabel();
+			labelTitreClassement.setText("Classements des eleves");
+			labelTitreClassement.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		/** Placement du labelTitreProf */
+		GridBagConstraints gbc = new GridBagConstraints();
+			gbc.gridx = gbc.gridy = 0; 							
+			gbc.gridheight = 2; 								
+			gbc.insets = new Insets(60, 0, 20, 0); 				
+		
+		panel1P.add(buttonRetour);
+		panel1P.add(labelTitreClassement, gbc);
+		
+		panelClassement.add(panel1P, BorderLayout.NORTH);
+		panelBodyCard.add(panelClassement, "ClassementPage");
+		
+		panel.add(panelBodyCard, BorderLayout.CENTER); /** au centre */
 		setContentPane(panel);
 		return panel;
 	}
@@ -253,26 +309,33 @@ public class EvalFenetre extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		Object  source = e.getSource();
 		
-		
 		if  (source == buttonProfesseur) {
-			
 			System.out.println("Professeur..");
 			professeur(panel);
 			
 			CardLayout cl = (CardLayout)(panelBodyCard.getLayout()); //implimented the change respectivly
-		     cl.show(panelBodyCard, e.getActionCommand());
-		     cl.show(panelBodyCard, "ProfesseurPage");
-			
-			
-			
+		     cl.show(panelBodyCard, "ProfesseurPage");	
 		}
 		else if(source == buttonEleve) {
 			System.out.println("Eleve..");
 			eleve(panel);
+			
+			CardLayout cl = (CardLayout)(panelBodyCard.getLayout()); //implimented the change respectivly
+		    cl.show(panelBodyCard, "ElevePage");
 		}
 		else if(source == buttonClassement) {
 			System.out.println("Classement..");
 			classement(panel);
+			
+			CardLayout cl = (CardLayout)(panelBodyCard.getLayout()); //implimented the change respectivly
+		    cl.show(panelBodyCard, "ClassementPage");
+		}
+
+		if(source == buttonRetour) {
+			System.out.println("Retour à la page d'accueil..");
+
+			CardLayout cl = (CardLayout)(panelBodyCard.getLayout()); //implimented the change respectivly
+		    cl.show(panelBodyCard, "FistPage");
 		}
 		
 	}
