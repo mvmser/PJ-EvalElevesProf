3926f014fe17c3f2a649d70b65c181f8c44ed762
Mohamed Serhir <32732921+mvmser@users.noreply.github.com>
2019-04-07 15:59:06 +0200

    delete author

--- src/notesElevesProfesseurs/Personne.java
+++ src/notesElevesProfesseurs/Personne.java
@@ -4,7 +4,6 @@ package notesElevesProfesseurs;
  * Eleve et Professeur heritent de Personne car ils ont des proprietes communes (prenom et nom)
  * On utilise alors la fonctionnalite d'heritage.
  * 
- * @author SERHIR, ZARGA
  * @version 1.0
  */
 public abstract class Personne {
--- src/notesElevesProfesseurs/Professeur.java
+++ src/notesElevesProfesseurs/Professeur.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 
- * @author SERHIR, ZARGA
  * @version 1.2
  *
  */
